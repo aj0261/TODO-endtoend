3b15cf5def8dc0e15e23e358d3e92fe30a2fd78a
Ajay Kumar Meena <ajay8076177390@gmail.com>
2024-06-25 18:53:42 +0530

    added useeffect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,25 @@
-import { useState } from 'react'
-import { CreateTodo } from './components/CreateTodo'
-import { Todos } from './components/Todos'
+import { useState, useEffect } from 'react';
+import { CreateTodo } from './components/CreateTodo';
+import { Todos } from './components/Todos';
+
 function App() {
-  const [todos,setTodos] = useState([]);
-  fetch("http://localhost:3000/todos")
-  .then(async function(res){
-    const json = await res.json();
-    setTodos(json.todos);
-  })
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/todos")
+      .then(async function(res) {
+        const json = await res.json();
+        setTodos(json.todos);
+      })
+      .catch(err => console.error('Error fetching todos:', err));
+  }, []);
+
   return (
-      <div>
-       <CreateTodo>
-        
-       </CreateTodo>
-       <Todos todos = {todos} ></Todos>
-      </div>
-  )
+    <div>
+      <CreateTodo />
+      <Todos todos={todos} />
+    </div>
+  );
 }
 
-export default App
+export default App;
